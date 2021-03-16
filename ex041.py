cf0a559b93c62d6aa627ec699d53b170dffdeffd
Caixeta <PAULO.CAIXETA@tetratech.com>
2021-03-16 00:24:21 -0300

    Create ex041.py

---
+++ ex041.py
@@ -0,0 +1,16 @@
+# Define categorias de natação com base na idade.
+from datetime import date
+nascimento = int(input('Digite a data de nascimento do atleta: '))
+idade = date.today().year - nascimento
+print('Idade: {}'.format(idade))
+print('A Categoria do atleta é: ')
+if idade < 9:
+    print('\033[1mMirim\033[m')
+elif idade >= 9 and idade < 14:
+    print('\033[1mInfantil\033[m')
+elif idade >= 14 and idade < 19:
+    print('\033[1mJunior\033[m')
+elif idade >= 19 and idade < 20:
+    print ('\033[1mSênior\033[m')
+else:
+    print('\033[1mMaster\033[m')
