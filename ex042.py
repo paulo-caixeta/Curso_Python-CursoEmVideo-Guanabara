d78d3534a8d663389c4cddb5e3b63f970e553425
Caixeta <PAULO.CAIXETA@tetratech.com>
2021-03-16 23:25:17 -0300

    resolução aula 11 (42 ao 45)

---
+++ ex042.py
@@ -0,0 +1,22 @@
+print('Digite a seguir 3 comprimentos de retas:')
+a = int(input('Reta a: '))
+b = int(input('Reta b: '))
+c = int(input('Reta c: '))
+modulo = b-c
+if modulo < 0:
+    modulo = modulo * (-1)
+if modulo < a and a < (b+c):
+    print('Estas retas podem formar um triângulo.')
+    if a == b and a == c:
+        print('O triângulo formado é \033[1mEQUILÁTERO\033[m')
+    elif a != b and a != c:
+        print('O triângulo formado é \033[1mESCALENO\033[m')
+    else:
+        print('O triangulo formado é \033[1mISÓCELES\033[m')
+else:
+    print('Estas retas NÃO podem formar um triângulo.')
+
+
+# Equilátero: todos os lados iguais
+# Isósceles: dois lados iguais
+# Escaleno: todos os lados diferentes
